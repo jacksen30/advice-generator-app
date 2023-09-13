cf28e289c7b1a5db7a5573c667a2cc36a4f6367e
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-09-13 18:17:34 +0800

    update script.js API fetching function to include error handling

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-const quoteP = document.getElementById('quote-text');
-const idP = document.getElementById('idP');
+const dynamicIdContainer = document.getElementById('dynamicId');
+const dynamicQuoteContainer = document.getElementById('dynamicQuote');
 
+const updateQuoteBtn = document.getElementById('updateQuoteBtn').addEventListener("click", retriveQuote);
 
+function retriveQuote () {
+    fetch('https://api.adviceslip.com/advice')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok")
+            }
+            return response.json();
+        })
+        .then(data => {
+            dynamicIdContainer.innerHTML = `Advice # ${data.slip.id}`;
+            dynamicQuoteContainer.innerHTML = `&ldquo;${data.slip.advice}&rdquo;`;
+        })
+        .catch(error => {
+            console.log('There was a problem with the fetch operation:', error.message);
+        });
+    };
 
-fetch('https://api.adviceslip.com/advice')
-    .then(response => response.json())
-    .then(data => {
-        idP.innerHTML = data.slip.id;
-        quoteP.innerHTML = `" ${data.slip.advice} "`;
-    });
-
-
-
-
-// .then(data => console.log(data.slip.advice, data.slip.id));
+retriveQuote();
