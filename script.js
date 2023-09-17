2cde2e76c34d683f4f45837ad0966c5233257158
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-09-17 15:04:57 +0800

    add glow affect for button on hover for desktop, added button rotation animation while data is been fetched

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const dynamicIdContainer = document.getElementById('dynamicId');
 const dynamicQuoteContainer = document.getElementById('dynamicQuote');
+const button = document.getElementById("updateQuoteBtn");
 
 const updateQuoteBtn = document.getElementById('updateQuoteBtn').addEventListener("click", retriveQuote);
 
 function retriveQuote () {
+    button.style.animation = "rotate 1s linear infinite";
+
     fetch('https://api.adviceslip.com/advice')
         .then(response => {
             if (!response.ok) {
@@ -14,9 +17,11 @@ function retriveQuote () {
         .then(data => {
             dynamicIdContainer.innerHTML = `Advice # ${data.slip.id}`;
             dynamicQuoteContainer.innerHTML = `&ldquo;${data.slip.advice}&rdquo;`;
+            button.style.animation = 'none';
         })
         .catch(error => {
             console.log('There was a problem with the fetch operation:', error.message);
+            button.style.animation = 'none';
         });
     };
 
