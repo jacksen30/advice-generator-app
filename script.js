179580dc20943b8909cb35dfc8ec2e12930f7ad3
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-09-17 16:41:06 +0800

    add screenshots of finished design, updated readme.md, commented script.js rotation animation code

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const button = document.getElementById("updateQuoteBtn");
 const updateQuoteBtn = document.getElementById('updateQuoteBtn').addEventListener("click", retriveQuote);
 
 function retriveQuote () {
+    // Start the rotation animation
     button.style.animation = "rotate 1s linear infinite";
 
     fetch('https://api.adviceslip.com/advice')
@@ -17,10 +18,12 @@ function retriveQuote () {
         .then(data => {
             dynamicIdContainer.innerHTML = `Advice # ${data.slip.id}`;
             dynamicQuoteContainer.innerHTML = `&ldquo;${data.slip.advice}&rdquo;`;
+            // Stop the rotation animation when data is received
             button.style.animation = 'none';
         })
         .catch(error => {
             console.log('There was a problem with the fetch operation:', error.message);
+            // Stop the rotation animation upon a fetch error
             button.style.animation = 'none';
         });
     };
